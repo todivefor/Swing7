c439c2f53a9a6701c20fc6d3ac352b29e2bce15f
Peter Ream <todivefor@gmail.com>
2022-05-13 17:38:09 -0400

    Fixed an inadvertent screwup. Add comments to FormPanel.

--- src/main/java/org/todivefor/swing7/FormPanel.java
+++ src/main/java/org/todivefor/swing7/FormPanel.java
@@ -48,14 +48,14 @@ public class FormPanel extends JPanel {
         
         GridBagConstraints gc = new GridBagConstraints();
         
-        ///// First row /////
-        gc.gridx = 0;
-        gc.gridy = 0;
-        gc.weightx = 1;
-        gc.weighty = 0.1;
         gc.fill = GridBagConstraints.NONE;
         
+        ///// First row /////
         // 0, 0
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.weightx = 1;                                                             // Relative width of cell
+        gc.weighty = 0.1;                                                           // Relative height of cell
         gc.anchor = GridBagConstraints.LINE_END;                                    // Align cell right
         gc.insets = new Insets(0, 0, 0, 5);                                         // Add spacing between label and field
         add(nameLabel, gc);
@@ -65,14 +65,6 @@ public class FormPanel extends JPanel {
         gc.gridy = 0;                                                               // Not needed, for clarity
         gc.weightx = 1;                                                             // Relative width of cell
         gc.weighty = 0.1;                                                           // Relative height of cells
-        gc.insets = new Insets(0, 0, 0, 5);
-        add(nameLabel, gc);
-        
-        // 1, 0
-        gc.gridx = 1;
-        gc.gridy = 0;                                                               // Not needed, for clarity
-        gc.weightx = 1;
-        gc.weighty = 0.1;
         gc.anchor = GridBagConstraints.LINE_START;
         gc.insets = new Insets(0, 0, 0, 0);                                         // Reset
         add(nameField,gc);
@@ -81,6 +73,8 @@ public class FormPanel extends JPanel {
         // 0, 1
         gc.gridx = 0;
         gc.gridy = 1;
+        gc.weightx = 1;                                                             // Relative width of cell
+        gc.weighty = 0.1; 
         gc.anchor = GridBagConstraints.LINE_END;
         gc.insets = new Insets(0, 0, 0, 5);
         add(occupationLabel, gc);
@@ -88,6 +82,8 @@ public class FormPanel extends JPanel {
         // 1, 1
         gc.gridx = 1;
         gc.gridy = 1;
+        gc.weightx = 1;                                                             // Relative width of cell
+        gc.weighty = 0.1; 
         gc.anchor = GridBagConstraints.LINE_START;
         gc.insets = new Insets(0, 0, 0, 0);
         add(occupationField, gc);
@@ -97,7 +93,7 @@ public class FormPanel extends JPanel {
         gc.gridx = 1;
         gc.gridy = 2;
         gc.weightx = 1;
-        gc.weighty = 2.0;                                                           // Reative height, bigger cell
+        gc.weighty = 1.0;                                                           // Reative height, bigger cell
         gc.anchor = GridBagConstraints.FIRST_LINE_START;                            // Top right of cell
 //        gc.anchor = GridBagConstraints.LAST_LINE_START;                             // Bottom right of cell
         gc.insets = new Insets(10, 0, 0, 0);                                        // Space (OK) down a little
